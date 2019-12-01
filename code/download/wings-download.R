5937b08b9624c6c23f641a820e8f12c6c2ebd99b
Ed Jee <edjee96@gmail.com>
2019-11-30 21:42:16 -0500

    wings now unzips data too

--- code/download/wings-download.R
+++ code/download/wings-download.R
@@ -1,2 +1,6 @@
 download.file("https://dataverse.harvard.edu/api/access/datafile/2725073",
               destfile = "data/wings-data.zip")
+
+unzip("data/wings-data.zip",
+      exdir = "data/wings-data/",
+      overwrite = TRUE)
